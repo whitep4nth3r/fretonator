51d6e951c4c11c37b4bd098fb2f52ccf3b9c5238
Salma Alam-Naylor <salma.alam.naylor@codecomputerlove.com>
2020-05-19 10:18:16 +0100

    moved service mock to test constants file

--- src/app/pages/home/home-index/generate-fret-map-pipe.spec.ts
+++ src/app/pages/home/home-index/generate-fret-map-pipe.spec.ts
@@ -1,22 +1,8 @@
 import { GenerateFretMapPipe } from './generate-fret-map.pipe';
 import { Mode } from '../../../util/types'
-import { GenerateFretMapService } from '../../../common/generate-fret-map/generate-fret-map.service'
+import { GenerateFretMapServiceMock } from '../../../common/generate-fret-map/generate-fret-map.service.testConstants'
 
 describe('GenerateFretMapPipe', () => {
-  const GenerateFretMapServiceMock: GenerateFretMapService = {
-    isNatural: jest.fn(),
-    isSharp: jest.fn(),
-    isFlat: jest.fn(),
-    isDoubleFlat: jest.fn(),
-    isDoubleSharp: jest.fn(),
-    generateNextNote: jest.fn(),
-    generateMode: jest.fn(),
-    convertNoteObjectToHumanReadable: jest.fn(),
-    convertNoteToFretMapKey: jest.fn(),
-    getFretMapping: jest.fn(),
-    sortFretMapping: jest.fn()
-   };
-
 
   it('create an instance', () => {
     const pipe = new GenerateFretMapPipe(GenerateFretMapServiceMock);
