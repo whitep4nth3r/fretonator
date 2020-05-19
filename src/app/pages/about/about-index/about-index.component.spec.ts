522485152fd66a6418be6b7c5b8220e3c0419501
Salma Alam-Naylor <salma.alam.naylor@codecomputerlove.com>
2020-05-19 10:52:59 +0100

    put some older test files back

---
+++ src/app/pages/about/about-index/about-index.component.spec.ts
@@ -0,0 +1,24 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { AboutIndexComponent } from './about-index.component';
+
+// describe('AboutIndexComponent', () => {
+//   let component: AboutIndexComponent;
+//   let fixture: ComponentFixture<AboutIndexComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [AboutIndexComponent],
+//     }).compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AboutIndexComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
