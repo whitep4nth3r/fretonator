66576cfe897acc1046df93480d47fe298668ee04
Jim Drury <jim@jimdrury.co.uk>
2020-05-20 08:46:21 +0100

    refactor(not from fret map pipe): refacotring so we dont need separate scale degree pipe

--- src/app/common/fretonator/fretonator.module.ts
+++ src/app/common/fretonator/fretonator.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FretonatorComponent } from './fretonator.component';
-import { GetNoteFromFretMapPipe } from './get-note-from-fret-map.pipe';
-import { GetScaleDegreeFromFretMapPipe } from './get-scale-degree-from-fret-map.pipe';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FretonatorComponent} from './fretonator.component';
+import {GetFretFromFretMapPipe} from './get-fret-from-fret-map.pipe';
 
 @NgModule({
-  declarations: [FretonatorComponent, GetNoteFromFretMapPipe, GetScaleDegreeFromFretMapPipe],
-  imports: [CommonModule],
-  exports: [FretonatorComponent],
+  declarations: [
+    FretonatorComponent,
+    GetFretFromFretMapPipe,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FretonatorComponent
+  ],
 })
-export class FretonatorModule {}
+export class FretonatorModule {
+}
