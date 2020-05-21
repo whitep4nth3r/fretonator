b73a642926e8460f32ed1708df7604436661a5d1
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-05-21 14:11:44 +0100

    Added two more jam tracks

--- src/app/data/jamTracks.ts
+++ src/app/data/jamTracks.ts
@@ -7,5 +7,20 @@ export const JamTracksData: JamTracks = [
     youTubeId: 'oMZz2FeV8q4',
     title: 'C Ionian (Major) Jam Track',
     creator: 'Tom Strahle',
-  }
+  },
+  {
+    key: NoteSymbol.d,
+    mode: Mode.dorian,
+    youTubeId: 'EIpG8eeFL0U',
+    title: 'Cool Chill Groove Backing Track D Dorian',
+    creator: 'YT Jam Tracks',
+  },
+  {
+    key: NoteSymbol.e,
+    mode: Mode.mixolydian,
+    youTubeId: 'j_N-aJTWfYk',
+    title: 'E Mixolydian Mode Majestic Mood Groove Backing Track',
+    creator: 'Quist',
+  },
 ];
+
