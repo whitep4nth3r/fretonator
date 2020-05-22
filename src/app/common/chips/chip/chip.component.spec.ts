40086ec9ec101a09366a91ff9d5b7f76e85506d0
Salma Alam-Naylor <salma.alam.naylor@codecomputerlove.com>
2020-05-22 09:17:20 +0100

    WIp tests

--- src/app/common/chips/chip/chip.component.spec.ts
+++ src/app/common/chips/chip/chip.component.spec.ts
@@ -1,16 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, DebugElement } from '@angular/core';
 import { ChipComponent } from './chip.component';
+import { ChipsModule } from './../chips.module';
 
 describe('ChipComponent', () => {
+  @Component({
+    selector: 'app-chip-spec',
+    template: ` <app-chips
+      ><app-chip></app-chip><app-chips></app-chips
+    ></app-chips>`,
+  })
+  class ChipComponentSpec {}
+
   let component: ChipComponent;
   let fixture: ComponentFixture<ChipComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChipComponent ]
-    })
-    .compileComponents();
+      declarations: [ChipComponentSpec],
+      imports: [ChipsModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/common/chips/chips.component.spec.ts
+++ src/app/common/chips/chips.component.spec.ts
@@ -1,16 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, DebugElement } from '@angular/core';
+import { ChipsModule } from './chips.module';
 import { ChipsComponent } from './chips.component';
 
 describe('ChipsComponent', () => {
+  @Component({
+    selector: 'app-chips-spec',
+    template: `
+      <app-chips>
+        <app-chip
+          [id]="id"
+          [value]="thing"
+          [disabled]="false"
+          [rounded]="false"
+        ></app-chip>
+      </app-chips>
+    `,
+  })
+  class ChipsComponentSpec {}
+
   let component: ChipsComponent;
   let fixture: ComponentFixture<ChipsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChipsComponent ]
-    })
-    .compileComponents();
+      declarations: [ChipsComponentSpec],
+      imports: [ChipsModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
