e89e176f6b72e126a83f02447cb90f89e1435f14
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-02 22:50:37 +0100

    Update tests after dynamic routes refactor

--- apps/fretonator-web/src/app/common/is-valid-mode-configuration/is-valid-mode-configuration.guard.ts
+++ apps/fretonator-web/src/app/common/is-valid-mode-configuration/is-valid-mode-configuration.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
 import { Mode, NoteExtenderSymbol, NoteSymbol } from '../../util/types';
 
 @Injectable({
