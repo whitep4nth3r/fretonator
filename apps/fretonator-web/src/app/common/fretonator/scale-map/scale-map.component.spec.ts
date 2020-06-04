672b9a2df4f62da794987d3e59193fabb3e7b854
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-04 14:17:36 +0100

    Fixed tests for new components

--- apps/fretonator-web/src/app/common/fretonator/scale-map/scale-map.component.spec.ts
+++ apps/fretonator-web/src/app/common/fretonator/scale-map/scale-map.component.spec.ts
@@ -1,20 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScaleMapComponent } from './scale-map.component';
+import { Component } from '@angular/core';
+import { ScaleMapModule } from './scale-map.module';
 
 describe('ScaleMapComponent', () => {
-  let component: ScaleMapComponent;
-  let fixture: ComponentFixture<ScaleMapComponent>;
+
+  @Component({
+    selector: 'app-scale-map-spec',
+    template: `
+      <app-scale-map></app-scale-map>
+    `,
+  })
+  class ScaleMapComponentSpec {
+  }
+
+  let component: ScaleMapComponentSpec;
+  let fixture: ComponentFixture<ScaleMapComponentSpec>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScaleMapComponent ]
+      declarations: [ ScaleMapComponentSpec ],
+      imports: [ ScaleMapModule ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ScaleMapComponent);
+    fixture = TestBed.createComponent(ScaleMapComponentSpec);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
