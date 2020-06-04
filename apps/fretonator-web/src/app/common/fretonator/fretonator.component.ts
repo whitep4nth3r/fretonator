356179295430f1d55e115920412f86cbb6c73a60
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-04 20:21:15 +0100

    Clean up fretonator file

--- apps/fretonator-web/src/app/common/fretonator/fretonator.component.ts
+++ apps/fretonator-web/src/app/common/fretonator/fretonator.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ChordMap, FretMap, Mode, Scale } from '../../util/types';
 
 @Component({
@@ -6,15 +6,11 @@ import { ChordMap, FretMap, Mode, Scale } from '../../util/types';
   templateUrl: './fretonator.component.html',
   styleUrls: ['./fretonator.component.scss'],
 })
-export class FretonatorComponent implements OnInit {
+export class FretonatorComponent {
   @Input() fretMap: FretMap;
   @Input() scale: Scale;
   @Input() intervalMap: [];
   @Input() chordMap: ChordMap;
   @Input() mode: Mode;
   frets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
