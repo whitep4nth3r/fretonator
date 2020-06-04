6b4b65e7467f937d590a98efa34e60c35229d962
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-04 20:26:17 +0100

    File cleanup

--- apps/fretonator-web/src/app/common/route-to-mode-configuration/route-to-mode-configuration.service.ts
+++ apps/fretonator-web/src/app/common/route-to-mode-configuration/route-to-mode-configuration.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Mode, NoteExtenderString, NoteSymbol } from '../../util/types';
 
+interface ModeRouteData {
+  note: NoteSymbol,
+  noteExtender: NoteExtenderString,
+  mode: Mode
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class RouteToModeConfigurationService implements Resolve<any> {
+export class RouteToModeConfigurationService implements Resolve<ModeRouteData> {
 
-  constructor() { }
-
-  resolve(route: ActivatedRouteSnapshot) {
+  resolve(route: ActivatedRouteSnapshot): ModeRouteData {
     return {
       note: route.params.note || NoteSymbol.c,
       noteExtender: route.params.noteExtender || NoteExtenderString.natural,
