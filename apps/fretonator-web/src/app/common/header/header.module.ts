2addfded75a1c29feda14f135241a9be51e09f83
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-17 14:58:44 +0100

    Condense mobile header with a logo no text component

--- apps/fretonator-web/src/app/common/header/header.module.ts
+++ apps/fretonator-web/src/app/common/header/header.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
 import { LogoWithTextModule } from '../svgs/logo-with-text/logo-with-text.module';
 import { LogoWithTextComponent } from '../svgs/logo-with-text/logo-with-text.component';
+import { LogoNoTextModule } from '../svgs/logo-no-text/logo-no-text.module';
 
 @NgModule({
   declarations: [HeaderComponent],
@@ -11,6 +12,7 @@ import { LogoWithTextComponent } from '../svgs/logo-with-text/logo-with-text.com
     CommonModule,
     RouterModule,
     LogoWithTextModule,
+    LogoNoTextModule
   ],
   exports: [
     HeaderComponent,
