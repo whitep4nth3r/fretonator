ccaccc69eee7b75ba64233f0e94312388f5e0a3d
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-18 13:11:58 +0100

    Fixed tests for contact success

--- apps/fretonator-web/src/app/pages/contact/contact-success/contact-success.component.spec.ts
+++ apps/fretonator-web/src/app/pages/contact/contact-success/contact-success.component.spec.ts
@@ -1,20 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactSuccessComponent } from './contact-success.component';
+import { Component } from '@angular/core';
+import { ContactModule } from '../contact.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ContactSuccessComponent', () => {
-  let component: ContactSuccessComponent;
-  let fixture: ComponentFixture<ContactSuccessComponent>;
+  @Component({
+    selector: 'app-contact-success-spec',
+    template: `
+      <app-contact-success></app-contact-success>
+    `
+  })
+  class ContactSuccessComponentSpec {
+  }
+
+  let component: ContactSuccessComponentSpec;
+  let fixture: ComponentFixture<ContactSuccessComponentSpec>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ContactSuccessComponent]
-    })
-      .compileComponents();
+      declarations: [ContactSuccessComponentSpec],
+      imports: [
+        ContactModule,
+        RouterTestingModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContactSuccessComponent);
+    fixture = TestBed.createComponent(ContactSuccessComponentSpec);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
