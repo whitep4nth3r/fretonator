4c5a14b14ad344060320f4c459868f2cf43e3ae5
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-18 13:02:18 +0100

    Styled and added content for success page

--- apps/fretonator-web/src/app/pages/contact/contact-success/contact-success.component.ts
+++ apps/fretonator-web/src/app/pages/contact/contact-success/contact-success.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GlobalService } from '../../../global.service';
 
 @Component({
   selector: 'app-contact-success',
   templateUrl: './contact-success.component.html',
   styleUrls: ['./contact-success.component.scss']
 })
-export class ContactSuccessComponent implements OnInit {
+export class ContactSuccessComponent {
 
-  constructor() {
+  constructor(private globalService: GlobalService) {
   }
 
-  ngOnInit(): void {
+  clickLink() {
+    this.globalService.getScrollTarget().scrollIntoView();
   }
-
 }
