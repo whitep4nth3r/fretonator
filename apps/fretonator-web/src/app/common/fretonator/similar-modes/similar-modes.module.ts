599d0a14b45c76cc695f812ebb4c490e7d0d2282
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-06-20 22:48:32 +0100

    Add a 'learn more' button to mode patterns block on home

--- apps/fretonator-web/src/app/common/fretonator/similar-modes/similar-modes.module.ts
+++ apps/fretonator-web/src/app/common/fretonator/similar-modes/similar-modes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SimilarModesComponent } from './similar-modes.component';
 import { ChipsModule } from '../../chips/chips.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ChipsModule } from '../../chips/chips.module';
   ],
   imports: [
     CommonModule,
-    ChipsModule
+    ChipsModule,
+    RouterModule
   ]
 })
 export class SimilarModesModule {
