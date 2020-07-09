71b14a5f2ffc8500b98d228be43a12b20b2a69e0
Salma Alam-Naylor <salma.alam.naylor@codecomputerlove.com>
2020-07-09 19:55:53 +0100

    Make a consistent experience when looking at pentatonic scales as well

--- apps/fretonator-web/src/app/common/fretonator/scale-map/scale-map.module.ts
+++ apps/fretonator-web/src/app/common/fretonator/scale-map/scale-map.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScaleMapComponent } from './scale-map.component';
-import { DisplayScaleDegreesPipe } from './display-scale-degrees.pipe';
-import { GetEnharmonicEquivalentPipe } from '../get-enharmonic-equivalent.pipe';
 import { RouterModule } from '@angular/router';
-import { GetEnharmonicRouterLinkPipe } from '../get-enharmonic-router-link.pipe';
 import { CrossModule } from '../../svgs/cross/cross.module';
 import { PlayModule } from '../../svgs/play/play.module';
 import { ScaleDegreesModule } from '../scale-degrees/scale-degrees.module';
@@ -13,11 +10,9 @@ import { ScaleDegreesModule } from '../scale-degrees/scale-degrees.module';
 @NgModule({
   declarations: [
     ScaleMapComponent,
-    DisplayScaleDegreesPipe
   ],
   exports: [
     ScaleMapComponent,
-    DisplayScaleDegreesPipe
   ],
   imports: [
     CommonModule,
